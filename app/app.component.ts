4d7a1f4d71f439e12d593b725a33e1caeeed104e
Yoseph Anderson <yrganderson@gmail.com>
2017-01-04 23:26:21 -0700

    task form * form to add tasks * simple styling

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
 
+import { Task } from './model/task';
+
 @Component({
     moduleId: module.id,
     selector: 'my-app',
-    templateUrl: 'app.component.html'
+    templateUrl: 'app.component.html',
+    styleUrls: [ 'app.component.css' ]
 })
 
-export class AppComponent { }
+export class AppComponent {
+    private tasks = [
+        new Task(
+            "Buy a Monkey",
+            false
+        ),
+        new Task(
+            "Walk the turtle",
+            false
+        )
+    ]
+}
